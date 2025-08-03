6392a18c51ded68f2245a592064defa08e3733bc
Nolan Phan <149433038+exnonuser@users.noreply.github.com>
2025-08-03 06:29:39 -0700

    Mostly environment additions

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class InteractableObject : MonoBehaviour
+{
+    public bool playerInRange;
+
+    public string ItemName;
+ 
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Mouse0)&&playerInRange&& SelectionManager.Instance.onTarget)
+        {
+            Debug.Log("Item Added to Inventory");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
